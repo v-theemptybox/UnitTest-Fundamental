faea0c713b27eb710807f7569396da48dacb02d4
H71M17 <vinhnguyen.goid2@gmail.com>
2021-05-08 14:32:47 +0700

    add maxAndMean Test

--- ConsoleApplication1/Class1.cs
+++ ConsoleApplication1/Class1.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Class1
+    public class Class1
     {
+        public int MaxAndMean(int a, int b, int c, out double mean)
+        {
+            mean = (a + b + c) / 3.0;
+            int maximum = 0;
+            if (a > b)
+            {
+                if (a > c)
+                    maximum = a;
+                else
+                    maximum = b;
+            }
+            else if (b > c)
+                maximum = b;
+            else
+                maximum = c;
+            return maximum;
+        }
     }
 }
--- UnitTest1/UnitTest1.cs
+++ UnitTest1/UnitTest1.cs
@@ -9,6 +9,28 @@ namespace UnitTest1
         [TestMethod]
         public void TestMethod1()
         {
+            ConsoleApplication1.Class1 c = new ConsoleApplication1.Class1();
+            double act_mean = 0;
+            int act_max = c.MaxAndMean(5, 2, 3, out act_mean);
+
+            double exp_mean = 3.33;
+            int exp_max = 5;
+            Assert.AreEqual(exp_max, act_max, 0.1);
+            Assert.AreEqual(exp_mean, act_mean, 0.1);
         }
+        //Failed Test
+
+        //[TestMethod]
+        //public void TestMethod2()
+        //{
+        //    ConsoleApplication1.Class1 c = new ConsoleApplication1.Class1();
+        //    double act_mean = 0;
+        //    int act_max = c.MaxAndMean(5, 2, 7, out act_mean);
+
+        //    double exp_mean = 4.66;
+        //    int exp_max = 7;
+        //    Assert.AreEqual(exp_max, act_max, 0);
+        //    Assert.AreEqual(exp_mean, act_mean, 0);
+        //}
     }
 }
